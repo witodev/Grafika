836d2490b84e508e5cf871f2a56d1ee5f4b17507
witodev <szwed.witold@gmail.com>
2019-02-26 20:28:42 +0100

    trackball source

--- GraphicControl/TrackBall.cs
+++ GraphicControl/TrackBall.cs
@@ -1,3 +1,5 @@
+// // taken from: https://github.com/Microsoft/WPF-Samples/blob/master/Sample%20Applications/HexSphereDemo/TrackBall.cs
+
 // // Copyright (c) Microsoft. All rights reserved.
 // // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
